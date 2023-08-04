a6163c5c902cfb554ce38beb3294007f6ab6e0e4
Carl <CarlMDev@outlook.com>
2023-08-04 14:05:33 +0300

    modified current day time

--- app/Http/Livewire/CurrentDayTimeTable.php
+++ app/Http/Livewire/CurrentDayTimeTable.php
@@ -5,12 +5,15 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Timerecord;
+use App\Models\TimeZone;
 use Illuminate\Support\Facades\DB;
+
 class CurrentDayTimeTable extends Component
 {
 
     public $records;
     public $userTz;
+    public $tz;
 
     protected $listeners = ['clockButtonClicked' => 'mount'];
 
@@ -18,20 +21,9 @@ class CurrentDayTimeTable extends Component
     {
         $userId = Auth::user()->id;
         $this->userTz = Auth::user()->time_zone;
-       // DB::connection()->enableQueryLog();
-        
-       $this->records = DB::select('SELECT * FROM time_records WHERE user_id = 1 AND created_at >= CONVERT_TZ(\'2023-08-03 00:00:00\',\'-08:00\',\'+00:00\');');
-       //$this->records = TimeRecord::where('user_id', '=', $userId)
-        //                    ->where('created_at', '>=', 'CONVERT_TZ(\'2023-08-03 00:00:00\',\'-08:00\',\'+00:00\') ')
-        // ->where('created_at', '>', 'CONVERT_TZ(\''. date('Y-M-d') . '\',\'' . $this->userTz . '\',\'+00:00\')  + INTERVAL 1 DAY ') //converting from local time to utc time to query database
-                      //  ->where('created_at', '<', 'CONVERT_TZ(CURRENT_DATE,\'+03:00\',\'+00:00\')  + INTERVAL 1 DAY')
-        //                      ->get();
+        $this->tz = TimeZone::where('name', '=', $this->userTz)->first();
 
-                            
-                             // $queries = \DB::getQueryLog();
-                          //    $last_query = end($queries);
-                             // dd($queries);
-        
+        $this->records = DB::select('SELECT * FROM time_records WHERE user_id = 1 AND created_at >= CONVERT_TZ(\''. date('Y-m-d') .  ' 00:00:00\',\'' . $this->tz->code . '\',\'+00:00\');');        
     }
 
     public function render()
--- resources/views/livewire/current-day-time-table.blade.php
+++ resources/views/livewire/current-day-time-table.blade.php
@@ -1,60 +1,70 @@
-<div class="flex flex-col items-center py-4">
-    <table class="table-autoborder-collapse border border-slate-400">
-        <thead>
-            <tr>
-                <th class="border border-slate-300 ...">Date</th>
-                <th class="border border-slate-300 ...">Time</th>
-                <th class="border border-slate-300 ...">Event</th>
-                <th class="border border-slate-300 ...">Hours</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php
-                {{ $hours = 0; }}
-                {{ $minutes = 0; }}
-                {{ $seconds = 0; }}
-                {{ $difference = 0; }}
-            @endphp
-            @for($i = 0; $i < sizeof($records); $i++) <tr>
-                <td class="px-2">
-                    @php
-                        {{$thisDate = new DateTime($records[$i]->created_at); }}    
-                    @endphp
-                    {{date_format($thisDate->setTimeZone(new DateTimeZone($userTz)),"m/d/Y");}}
-                </td>
-                <td class="px-2">
-                    {{date_format($thisDate->setTimeZone(new DateTimeZone($userTz)),"H:i");}}
-                </td>
-                <td class="px-2">
-                    @if($records[$i]->in_out == 0)
-                        OUT
-                    @else
-                        IN
-                    @endif
-                </td>
+<div class="flex justify-center pt-4">
+    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div class="overflow-hidden">
+                <table class="min-w-full text-left text-sm font-light">
+                    <thead class="border-b bg-white font-medium dark:border-neutral-500 dark:bg-neutral-600">
+                        <tr>
+                            <th scope="col" class="px-6 py-4">Date</th>
+                            <th scope="col" class="px-6 py-4">Time</th>
+                            <th scope="col" class="px-6 py-4">Event</th>
+                            <th scope="col" class="px-6 py-4">Total hours</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                            {{ $hours = 0; }}
+                            {{ $minutes = 0; }}
+                            {{ $seconds = 0; }}
+                            {{ $difference = 0; }}
+                        @endphp
+                        @for($i = 0; $i < sizeof($records); $i++) 
+                        <tr class="border-b bg-neutral-100 dark:border-neutral-500 dark:bg-neutral-700">
+                            <td class="whitespace-nowrap px-6 py-4 font-medium">
+                                @php
+                                    {{$thisDate = new DateTime($records[$i]->created_at); }}
+                                @endphp
+                                
+                                {{date_format($thisDate->setTimeZone(new DateTimeZone($userTz)),"m/d/Y");}}
+                            </td>
+                            <td class="whitespace-nowrap px-6 py-4 font-medium">
+                                {{date_format($thisDate->setTimeZone(new DateTimeZone($userTz)),"H:i");}}
+                            </td>
+                            <td class="whitespace-nowrap px-6 py-4 font-medium">
+                                @if($records[$i]->in_out == 0)
+                                    OUT
+                                @else
+                                    IN
+                                @endif
+                            </td>
 
-                @if($records[$i]->in_out == 0 && $records[$i-1]->in_out == 1 && $i > 0)
-                    @php
-                        {{$firstDate = new DateTime($records[$i-1]->created_at); }}
-                        {{$secondDate = new DateTime($records[$i]->created_at); }}
-                        {{ $difference = date_diff($firstDate, $secondDate, true); }}
-                        {{ $hours += $difference->h; }}
-                        {{ $minutes += $difference->i; }}
-                        {{ $seconds += $difference->s; }}
-                        
-                        {{ $minutes += $seconds/60; }}
+                            @if($records[$i]->in_out == 0 && $records[$i-1]->in_out == 1 && $i > 0)
+                                @php
+                                    {{$firstDate = new DateTime($records[$i-1]->created_at); }}
+                                    {{$secondDate = new DateTime($records[$i]->created_at); }}
+                                    {{ $difference = date_diff($firstDate, $secondDate, true); }}
+                                    {{ $hours += $difference->h; }}
+                                    {{ $minutes += $difference->i; }}
+                                    {{ $seconds += $difference->s; }}
 
-                        {{ $hours += $minutes/60; }}
-                        {{ $hours = 0.01 * (int)($hours * 100); }}
-                    @endphp
-                @endif
+                                    {{ $minutes += $seconds/60; }}
 
-                <td class="px-2">
-                    {{ $hours }}
-                </td>
-                </tr>
-                @endfor
-        </tbody>
-    </table>
-    Total: {{$hours}} hours 
+                                    {{ $hours += $minutes/60; }}
+                                    {{ $hours = 0.01 * (int)($hours * 100); }}
+                                @endphp
+                            @endif
+
+                            <td class="whitespace-nowrap px-6 py-4 font-medium">
+                                {{ $hours }}
+                            </td>
+                        </tr>
+                            @endfor
+                    </tbody>
+                </table>
+                <div class="pt-3 flex justify-center">
+                    <span>Total: {{$hours}} hours</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
